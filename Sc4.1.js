7b198e762706d60dc63069970054a778e31fdeff
Mo0mer <104166536+Mo0mer@users.noreply.github.com>
2024-06-21 12:08:57 +0300

    Задание 4.1 Проверка на равенство

---
+++ Sc4.1.js
@@ -0,0 +1,11 @@
+let x = +(prompt("Введите X"));
+let y = +(prompt("Введите Y"));
+if (x > y){
+    alert("X больше");
+}
+else if (x < y){
+    alert("Y больше");
+}
+else{
+    alert("Равенство");
+}
